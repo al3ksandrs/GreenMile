433e713010caab4c7373ce0eb73f18284cab2c9d
Anouar T <anouar.tarahbi@hva.nl>
2023-03-06 09:47:44 +0100

    Drie opties voor vertaling:
    1. Google Translate API (is ontzettend lelijk)
    2 en 3.  Google Translate's web-API importeren via Node en met de documentatie() deze implementeren.
    
    (4. Direct vertalen via een javascript embed functie per onderdeel en pagina)

---
+++ src/assets/js/vertaling.js
@@ -0,0 +1,20 @@
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement({pageLanguage: 'nl', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+// }
+
+// import { setCORS } from "google-translate-api-browser";
+// // setting up cors-anywhere server address
+// const translate = setCORS("http://cors-anywhere.herokuapp.com/");
+// 
+
+// import translate, { setCORS } from "google-translate-api-browser";
+// setCORS("http://cors-anywhere.herokuapp.com/");
+// */
+// translate(element, { to: "en" })
+//   .then(res => {
+//     // I do not eat six days
+//     console.log(res.text)
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
