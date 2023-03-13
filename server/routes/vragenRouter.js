6ab25f464a8706318e0044b2016703c1a77be4c7
sakhi <sakhi.anwari@hva.nl>
2023-03-13 11:13:04 +0100

    vragenrouter geupdatet

---
+++ server/routes/vragenRouter.js
@@ -0,0 +1,13 @@
+/**
+ * Router files for the frequently asked question page
+ * @author Sakhi Anwari
+ */
+
+class VragenRouter{
+    #app;
+
+    constructor(app) {
+        this.#app = app;
+
+    }
+}
