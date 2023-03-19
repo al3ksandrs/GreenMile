a5fcd524cce2c84fe24d5f586b4991db690bc23d
Aleksandrs Soskolainens <SOSKOLA@ad.hva.nl>
2023-03-19 23:41:00 +0100

    [#16] Basic admin pagina opgezet met bijhoorende forms

---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,41 @@
+/**
+ * This class contains ExpressJS routes specific for the users entity
+ * this file is automatically loaded in app.js
+ *
+ * route file for admin
+ */
+
+class adminRoutes {
+    #errorCodes = require("../framework/utils/httpErrorCodes");
+    #databaseHelper = require("../framework/utils/databaseHelper");
+    #app;
+
+    constructor(app) {
+        this.#app = app;
+
+        this.#admin();
+    }
+
+    #admin() {
+        this.#app.post("/admin", async(req, res) => {
+
+            const type = req.body.addGreenType;
+
+            try {
+                let data = await this.#databaseHelper.handleQuery( {
+                    query: "INSERT INTO type(naam) VALUES (?)",
+                    values: [type]
+                });
+
+                res.status(this.#errorCodes.HTTP_OK_CODE).json(data);
+
+            } catch (e) {
+                res.status(this.#errorCodes.BAD_REQUEST_CODE).json({reason: e});
+            }
+
+        });
+    }
+
+}
+
+module.exports = adminRoutes
