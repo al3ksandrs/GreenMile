b59112209becb053bb66287d704f8a3343db3594
Thijs Beers MBP <thijs.beers@hva.nl>
2023-03-27 15:08:23 +0200

    Rebase fix ofz

--- src/assets/js/repositories/adminRepository.js
+++ src/assets/js/repositories/adminRepository.js
@@ -1,38 +1,15 @@
 import {NetworkManager} from "../framework/utils/networkManager.js";
 
 export class AdminRepository {
-    #networkmanager;
-    #adminRoutes;
-    #areaListRoute;
-    #typeListRoute;
-    #addGreenRoute;
+    #networkManager;
+    #lkiRoute;
 
     constructor() {
-        this.#networkmanager = new NetworkManager();
-        this.#adminRoutes = "/admin";
-        this.#areaListRoute = "/areaList"
-        this.#typeListRoute = "/typeList"
-        this.#addGreenRoute = "/adminAddGreen"
+        this.#lkiRoute = "/lki";
+        this.#networkManager = new NetworkManager();
     }
 
-    async addGreen(coordinaatX, coordinaatY, gebied_id, type_id) {
-        return await this.#networkmanager.doRequest(this.#addGreenRoute, "POST", {
-            "coordinaatX": coordinaatX, "coordinaatY": coordinaatY, "gebied_id" : gebied_id, "type_id" : type_id
-        });
+    getLKIvalues() {
+        return this.#networkManager.doRequest(this.#lkiRoute, "GET");
     }
-
-    async getArea() {
-        return await this.#networkmanager.doRequest(this.#areaListRoute, "GET");
-    }
-
-    async getType() {
-        return await this.#networkmanager.doRequest(this.#typeListRoute, "GET");
-    }
-
-    async addGreenType(type) {
-        return await this.#networkmanager.doRequest(this.#adminRoutes, "POST", {
-            "type": type
-        });
-    }
-
 }
